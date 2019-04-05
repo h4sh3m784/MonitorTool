70731b47fed6d6246369ee8b77cf0c2dda4672c5
Unknown <h4sh3m784@gmail.com>
2019-04-05 15:26:10 +0200

    lambda fix

--- serverless/handler.py
+++ serverless/handler.py
@@ -2,7 +2,7 @@ import json
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
 
-def handler(event, context):
+def handler(event, contedict):
     dynamodb = boto3.resource('dynamodb', region_name='eu-west-1')
     table = dynamodb.Table('ConnectedDevicesTable')
     scan = table.scan()
@@ -10,9 +10,8 @@ def handler(event, context):
 
     devices = {}
 
-    for x in data:
-        if x == "ID":
-            devices.append(x['ID'])
-        print(x)
+    for d in data:
+        devices.append(d['ID'])
+        print(d['ID'])
         
     return json.dumps({"Devices": devices})
